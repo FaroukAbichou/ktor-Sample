9b65d644a704a28e7b2921625760a3805274c524
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-07-31 18:21:46 +0100

    - Added get Cat by id

--- src/main/kotlin/com/example/routes/CatRoutes.kt
+++ src/main/kotlin/com/example/routes/CatRoutes.kt
@@ -1,12 +1,24 @@
 package com.example.routes
 
 import com.example.data.local.cats
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.catRouting() {
-    get("/cat") {
-        call.respondText(cats.toString())
+    route("/cat") {
+        get {
+            call.respond(cats)
+        }
+        get("{id?}") {
+            val id = call.parameters["id"]?.toIntOrNull()
+            val cat = cats.find { it.id == id }
+            if (cat == null) {
+                call.respondText("No cat with id $id", status = HttpStatusCode.BadRequest)
+            } else {
+                call.respond(cat)
+            }
+        }
     }
 }
